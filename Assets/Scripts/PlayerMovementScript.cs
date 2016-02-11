24b7698622203ebc4d8bef4ceed0b02eb59bf637
Don Demetrius <demetrius25_7@hotmail.com>
2016-02-11 00:53:19 +0000

    Proper ship models. Basic asteroid script.
    EOP 10-02-2016

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerMovementScript : MonoBehaviour {
 
-    public Vector3 m_initialDirection;
+    //public Vector3 m_initialDirection;
     public float m_rotationAmount;
     public float m_thrustAmount;
 
@@ -13,16 +13,17 @@ public class PlayerMovementScript : MonoBehaviour {
     {
         // Constrain to x-y axis
         m_rigidBody = GetComponent<Rigidbody>();
-        m_rigidBody.constraints = RigidbodyConstraints.FreezePositionZ;
+        //m_rigidBody.constraints = RigidbodyConstraints.FreezePositionZ;
 
-        transform.rotation = Quaternion.LookRotation(m_initialDirection);
+        //transform.rotation = Quaternion.LookRotation(m_initialDirection);
+        //m_rigidBody.transform.forward = new Vector3(1, 0, 0);
 	}
 	
 	void FixedUpdate () 
     {
         // Rotation
         float h = Input.GetAxis("Horizontal") * m_rotationAmount * Time.deltaTime;
-        Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, h, 0) * Time.deltaTime);
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(h, 0, 0) * Time.deltaTime);
         m_rigidBody.MoveRotation(m_rigidBody.rotation * deltaRotation);
 
         // Using torque instead makes it super hard
@@ -32,14 +33,14 @@ public class PlayerMovementScript : MonoBehaviour {
 
         // Thrust
         float t = Input.GetAxis("Vertical") * m_thrustAmount * Time.deltaTime;
-        m_rigidBody.AddForce(transform.forward * t);
-
+        //m_rigidBody.AddForce(transform.forward * t);
+        m_rigidBody.AddForce(m_rigidBody.transform.forward * t);
 
 
         // Fire bullets
         if ( Input.GetAxis("Fire1") == 1.0f )
         {
-            GameObject obj = (GameObject)Instantiate(Resources.Load("BulletPrefab"), m_rigidBody.transform.position, Quaternion.identity);
+            //GameObject obj = (GameObject)Instantiate(Resources.Load("BulletPrefab"), m_rigidBody.transform.position, Quaternion.identity);
             // TODO: Recycle bullets
             //obj.transform.forward = m_rigidBody.transform.forward;
         }
